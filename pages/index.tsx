617ac3a833b7056f9300b745bdac9cf62a1a6e50
NaokiYazawa <yazawa.naoki.01@gmail.com>
2022-02-07 15:11:58 +0900

    added resume link

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,8 @@ export const Index = ({ posts }: IndexProps): JSX.Element => {
           </p>
           <div className="pt-6">
             <a
-              href="#"
+              href="https://nervous-kepler-2fc28d.netlify.app/"
+              target="blank"
               className="bg-brand-button rounded-full inline-flex justify-center items-center space-x-2"
             >
               <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800">
