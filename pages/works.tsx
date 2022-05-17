ed75c459262c774f7c9a4c2b794939cf5d5834b5
NaokiYazawa <yazawa.naoki.01@gmail.com>
2022-05-17 10:43:48 +0900

    added a work

--- pages/works.tsx
+++ pages/works.tsx
@@ -37,19 +37,19 @@ export const Works = (): JSX.Element => {
             <div className="p-10 w-full lg:w-1/3 lg:p-4">
               <div className="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-16 rounded-lg overflow-hidden text-center relative">
                 <h2 className="tracking-widest text-xs title-font font-medium text-gray-600 mb-1">
-                  Blockchain
+                  Next.js
                 </h2>
                 <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
-                  Chat App
+                  Bookmark App
                 </h1>
                 <p className="leading-relaxed mb-5">
-                  I created Decentralized App using Nextjs and Moralis.
+                  I created a Bookmark App using Nextjs and TypeScript
                 </p>
                 <a
-                  href="https://metaverse-2vkqe7xgq-naokiyazawa.vercel.app/"
+                  href="https://bookmark-farstep.vercel.app/"
                   className=" inline-flex items-center"
                 >
-                  <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800">
+                  <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:ring-purple-200 dark:focus:ring-purple-800">
                     <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                       View
                     </span>
